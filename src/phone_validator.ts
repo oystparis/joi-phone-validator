cbd19e383752649b48da4bc06012fd93c2e8d4e6
Jordan Van Walleghem <jordan.vanwalleghem@oyst.com>
2017-06-20 16:59:54 +0200

    V2.0

---
+++ src/phone_validator.ts
@@ -0,0 +1,62 @@
+import * as sourceMapSupport from 'source-map-support'
+
+sourceMapSupport.install()
+
+import * as PhoneNumbers from 'google-libphonenumber'
+import * as Joi from 'joi'
+
+const phoneNumberUtil = PhoneNumbers.PhoneNumberUtil.getInstance()
+const PhoneNumberType = PhoneNumbers.PhoneNumberType
+const PNF = PhoneNumbers.PhoneNumberFormat
+
+export default Joi.extend({
+  base: Joi.string(),
+  language: {
+    mobile: 'needs to be a mobile phone number',
+    validate: 'needs to be a valid phone number according to E.164 international format'
+  },
+  name: 'phone',
+  // Weak Joi typings :(
+  pre (this: any, value: any, state: any, options: any) {
+    if (options.convert) {
+      try {
+        const phoneNumber = phoneNumberUtil.parse(value)
+        return phoneNumberUtil.format(phoneNumber, PNF.E164)
+      } catch (e) {
+        return this.createError('phone', { v: value }, state, options)
+      }
+    }
+    return value
+  },
+  rules: [{
+    name: 'mobile',
+    // Weak Joi typings bis :(
+    validate (this: any, _: any, value: any, state: any, options: any) {
+      try {
+        const phoneNumber = phoneNumberUtil.parse(value)
+        const isMobile = phoneNumberUtil.getNumberType(phoneNumber) === PhoneNumberType.MOBILE
+        if (!isMobile) {
+          return this.createError('phone.mobile', { v: value }, state, options)
+        }
+        return value
+      } catch (e) {
+        return this.createError('phone.mobile', { v: value }, state, options)
+      }
+    }
+  }, {
+    name: 'validate',
+    // Weak Joi typings ter :(
+    validate (this: any, _: any, value: any, state: any, options: any) {
+      try {
+        const phoneNumber = phoneNumberUtil.parse(value)
+        const isValid = phoneNumberUtil.isValidNumber(phoneNumber)
+        if (!isValid) {
+          return this.createError('phone.validate', { v: value }, state, options)
+        }
+        return value
+      } catch (e) {
+        return this.createError('phone.validate', { v: value }, state, options)
+      }
+    }
+  }]
+})
